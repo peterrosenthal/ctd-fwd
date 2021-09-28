0564ff334bf5247ab9124fd5ac04414346746a04
Peter Rosenthal <peter@rosenth.al>
2021-09-27 19:29:56 -0600

    add final end game state to lab 4

--- code/lab-4/main.js
+++ code/lab-4/main.js
@@ -4,7 +4,7 @@ Pts.namespace(window);
 const colors = ['#816e94', '#74226c', '#4b2142'];
 const shapes = ['circle', 'rectangle', 'triangle'];
 let points = 0;
-let step = 1;
+let combo = 1;
 let selection = {
   color: -1,
   shape: -1
@@ -36,6 +36,37 @@ function createRand3Order() {
   return order;
 }
 
+// function trackLayerWithClassList(el, colorArray, shapeArray) is a little hack I'm doing to use CSS classes to keep track (and store the state in an accessible manner) of the topmost layer of each canvas
+// I wouldn't need to do any of this if I had made my life a little bit easier by programming with an OOP mindset to begin with, but all the Pts.js examples I was reading were so functional and they looked so clean, that I wanted to try and do the same. But a functional-ish coding paradigm is *very* much not my strong suite.
+function trackLayerWithClassList(el, colorArray, shapeArray) {
+  el.classList.remove('color-layer-0');
+  el.classList.remove('color-layer-1');
+  el.classList.remove('color-layer-2');
+  el.classList.remove('shape-layer-0');
+  el.classList.remove('shape-layer-1');
+  el.classList.remove('shape-layer-2');
+  if (colorArray.length > 0 && shapeArray.length > 0) {
+    el.classList.add(`color-layer-${colorArray[colorArray.length - 1]}`);
+    el.classList.add(`shape-layer-${shapeArray[shapeArray.length - 1]}`);
+  }
+}
+
+// function checkForEligibleMoves(canvas, checkParent) checks the specified canvas aganist all other canvases on the DOM, and uses their CSS classes to determine if that canvas can be combo'd into another canvas (return true/false)
+// the checkParent bool flag determines wether the parent divs of the canvases that are being check are allowed to be "active" or not
+function checkForEligibleMoves(canvas, checkParent) {
+  let eligibleMoves = false;
+  document.querySelectorAll('canvas').forEach((documentCanvas) => {
+    if (canvas !== documentCanvas) {
+      if (canvas.className == documentCanvas.className && canvas.className != '') {
+        if (!checkParent || (checkParent && !documentCanvas.parentElement.classList.contains('active-container'))) {
+          eligibleMoves = true;
+        }
+      }
+    }
+  });
+  return eligibleMoves;
+}
+
 // function drawTile(canvas) sets up a canvas as a tile in this NYT Tiles inspired game
 function drawTile(canvas) {
   let space = new CanvasSpace(canvas);
@@ -47,16 +78,10 @@ function drawTile(canvas) {
   let canvasSize = new Pt();
   let potentialFutureRemoval = false;
 
-  canvas.classList.remove('color-layer-0');
-  canvas.classList.remove('color-layer-1');
-  canvas.classList.remove('color-layer-2');
-  canvas.classList.remove('shape-layer-0');
-  canvas.classList.remove('shape-layer-1');
-  canvas.classList.remove('shape-layer-2');
-  canvas.classList.add(`color-layer-${layerColors[layerColors.length - 1]}`);
-  canvas.classList.add(`shape-layer-${layerShapes[layerShapes.length - 1]}`);
+  trackLayerWithClassList(canvas, layerColors, layerShapes);
 
   space.add({
+    // function start(bound) is called once upon initializing the canvas, this is where most of the setup stuff goes
     start: (bound) => {
       canvasSize = bound.size;
       let margin = canvasSize.clone().scale(0.1);
@@ -65,19 +90,13 @@ function drawTile(canvas) {
       }
     },
 
+    // function animate() is called once per frame (like 60-ish times per seccond, give or take a some depending on how easy or hard it is to render each frame)
     animate: () => {
       if (removalEvent && potentialFutureRemoval) {
         layerColors.pop();
         layerShapes.pop();
         layerPositions.pop();
-        canvas.classList.remove('color-layer-0');
-        canvas.classList.remove('color-layer-1');
-        canvas.classList.remove('color-layer-2');
-        canvas.classList.remove('shape-layer-0');
-        canvas.classList.remove('shape-layer-1');
-        canvas.classList.remove('shape-layer-2');
-        canvas.classList.add(`color-layer-${layerColors[layerColors.length - 1]}`);
-        canvas.classList.add(`shape-layer-${layerShapes[layerShapes.length - 1]}`);
+        trackLayerWithClassList(canvas, layerColors, layerShapes);
         removalEvent = false;
         potentialFutureRemoval = false;
       }
@@ -110,6 +129,7 @@ function drawTile(canvas) {
       }
     },
 
+    // function action(type) is called every time a mouse event or touch event is triggered on the canvas
     action: (type) => {
       if (type == 'down') {
         if (selection.color == -1 && selection.shape == -1) {
@@ -124,27 +144,30 @@ function drawTile(canvas) {
         } else if (selection.color == layerColors[layerColors.length - 1]
                 && selection.shape == layerShapes[layerShapes.length - 1]
                 && !potentialFutureRemoval) {
-          points += step;
-          step++;
+          points += combo;
+          if (combo == 1) {
+            removalEvent = true;
+          }
+          if (checkForEligibleMoves(canvas, true)) {
+            combo++;
+            activatedStyle = true;
+            canvas.parentElement.classList.add('active-container');
+            space.background = '#8bceb0';
+            let colorNames = ['light', 'medium', 'dark'];
+            document.querySelector('.slot').innerHTML = `${combo}x on ${colorNames[selection.color]} ${shapes[selection.shape]}!`;
+          } else {
+            combo = 1;
+            selection = {
+              color: -1,
+              shape: -1
+            };
+            document.querySelector('.slot').innerHTML = 'None';
+            activatedStyle = false;
+          }
           layerColors.pop();
           layerShapes.pop();
           layerPositions.pop();
-          activatedStyle = true;
-          canvas.parentElement.classList.add('active-container');
-          space.background = '#8bceb0';
-          let colorNames = ['light', 'medium', 'dark'];
-          document.querySelector('.slot').innerHTML = `${step}x on ${colorNames[selection.color]} ${shapes[selection.shape]}!`;
-          canvas.classList.remove('color-layer-0');
-          canvas.classList.remove('color-layer-1');
-          canvas.classList.remove('color-layer-2');
-          canvas.classList.remove('shape-layer-0');
-          canvas.classList.remove('shape-layer-1');
-          canvas.classList.remove('shape-layer-2');
-          canvas.classList.add(`color-layer-${layerColors[layerColors.length - 1]}`);
-          canvas.classList.add(`shape-layer-${layerShapes[layerShapes.length - 1]}`);
-          if (step == 2) {
-            removalEvent = true;
-          }
+          trackLayerWithClassList(canvas, layerColors, layerShapes);
         } else {
           selection = {
             color: -1,
@@ -152,16 +175,33 @@ function drawTile(canvas) {
           };
           document.querySelector('.slot').innerHTML = 'None';
           activatedStyle = false;
-          if (step > 1) {
-            step = 1;
+          if (combo > 1) {
+            combo = 1;
           } else {
             points--;
           }
         }
-        console.log(points);
+        let gameWon = true;
+        let eligibleMoves = false;
+        document.querySelectorAll('canvas').forEach((documentCanvas) => {
+          if (documentCanvas.className.length > 0) {
+            gameWon = false;
+          }
+          if (checkForEligibleMoves(documentCanvas, false)) {
+            eligibleMoves = true;
+          }
+        });
+        if (gameWon) {
+          score *= 2;
+          document.querySelector('header').querySelector('p').innerHTML = `You win! You scored ${points} points!`;
+        }
+        if (!eligibleMoves) {
+          document.querySelector('header').querySelector('p').innerHTML = `Game over! You got ${points} points! Sometimes it's the luck of the draw, sometimes it's how you play your cards. Hit refresh to try again for a higer score!`;
+        }
       }
     },
 
+    // function resize(size) is called every time the canvas's size changes
     resize: (size) => {
       let scale = size.size.$divide(canvasSize);
       layerPositions.scale(scale);
@@ -169,7 +209,7 @@ function drawTile(canvas) {
     }
   });
 
-  space.bindMouse().play();
+  space.bindMouse().bindTouch().play();
 }
 
 document.querySelectorAll('canvas').forEach(canvas => drawTile(canvas));
